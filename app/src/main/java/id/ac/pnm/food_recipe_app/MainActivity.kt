579382cbfe332b78306ffaf6c4058d218577cfcb
Rofiqul_Anam <rofiqanam8@gmail.com>
2025-11-23 17:33:12 +0700

    bottom_navbar

--- app/src/main/java/id/ac/pnm/food_recipe_app/MainActivity.kt
+++ app/src/main/java/id/ac/pnm/food_recipe_app/MainActivity.kt
@@ -5,16 +5,55 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import id.ac.pnm.food_recipe_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        replaceFragment(Home_Fragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.Home -> replaceFragment(Home_Fragment())
+                R.id.Profile -> replaceFragment(Profile_Fragment())
+                R.id.Favorite -> replaceFragment(Fav_Fragment())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val framentManager = supportFragmentManager
+        val fragmentTransaction = framentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.Frame_Layout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
